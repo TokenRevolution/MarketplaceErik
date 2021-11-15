9ae6a60089d013b0a21b0297bea554d73c620252
TokenRevolution <94378706+TokenRevolution@users.noreply.github.com>
2021-11-15 20:56:05 +0100

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -2,15 +2,8 @@ var web3;
 
 async function Connect(){
 	await	window.web3.currentProvider.enable();
-	web3=new web3(window.web3.currentProvider)
+	web3=new web3(window.web3.currentProvider);
 }
-const ethereumButton = document.querySelector('.enableEthereumButton');
-const showAccount = document.querySelector('.showAccount');
-
-ethereumButton.addEventListener('click', () => {
-  getAccount();
-});
-
 async function getAccount() {
   const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
   const account = accounts[0];
